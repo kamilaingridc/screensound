df4413a568ad0659be53e65e54f0ee8da1c4ada3
Kamila Carvalho <kamilaingridc@gmail.com>
2025-04-28 16:56:29 -0300

    :tada: Initial commit

---
+++ src/main/java/br/com/ingrid/screensound/Main/Main.java
@@ -0,0 +1,67 @@
+package br.com.ingrid.screensound.Main;
+
+import java.util.Scanner;
+
+public class Main {
+    private Scanner scanner;
+
+    public void exibeMenu() {
+        var opcao = -1;
+
+        while (opcao!= 9) {
+            var menu = """
+                    *** Screen Sound Músicas ***                    
+                                        
+                    1- Cadastrar artistas
+                    2- Cadastrar músicas
+                    3- Listar músicas
+                    4- Buscar músicas por artistas
+                    5- Pesquisar dados sobre um artista
+                                    
+                    9 - Sair
+                    """;
+
+            System.out.println(menu);
+            opcao = scanner.nextInt();
+            scanner.nextLine();
+
+            switch (opcao) {
+                case 1:
+                    cadastrarArtistas();
+                    break;
+                case 2:
+                    cadastrarMusicas();
+                    break;
+                case 3:
+                    listarMusicas();
+                    break;
+                case 4:
+                    buscarMusicasPorArtista();
+                    break;
+                case 5:
+                    pesquisarDadosDoArtista();
+                    break;
+                case 9:
+                    System.out.println("Encerrando a aplicação!");
+                    break;
+                default:
+                    System.out.println("Opção inválida!");
+            }
+        }
+    }
+
+    private void pesquisarDadosDoArtista() {
+    }
+
+    private void buscarMusicasPorArtista() {
+    }
+
+    private void listarMusicas() {
+    }
+
+    private void cadastrarMusicas() {
+    }
+
+    private void cadastrarArtistas() {
+    }
+}
